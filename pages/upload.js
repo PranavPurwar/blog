8aaa8e8c3387b6fd31be287aaffeb32d48dc0dd3
PranavPurwar <purwarpranav80@gmail.com>
2023-03-07 13:36:20 +0530

    p

--- pages/upload.js
+++ pages/upload.js
@@ -9,26 +9,32 @@ export default function UploadPage() {
   };
 
   const handleUpload = async () => {
+    if (process.env.TOKEN) {
     const octokit = new Octokit({
       auth: process.env.TOKEN,
     });
 
     // Read the contents of the selected file
     const fileContents = await selectedFile.text();
+    alert(fileContents)
 
     // Upload the file to the repository
     try {
       await octokit.repos.createOrUpdateFileContents({
         owner: 'PranavPurwar',
         repo: 'blog',
-        path: selectedFile.name,
+        path: "file",
         message: `Upload ${selectedFile.name}`,
-        content: Buffer.from(fileContents).toString('base64'),
+        content: Buffer.from("hello world").toString('base64'),
       });
     } catch (error) {
       alert(error);
     }
+    } else {
+        alert("no token in env")
+    }
   };
+  }
 
   return (
     <div>
